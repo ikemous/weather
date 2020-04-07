c41be56c383fa3923fc1dc855585ca24c179688c
Ike Barranco <barranco.ike@outlook.com>
2020-04-07 03:33:53 -0600

    cleaned up javascript by created subfunctions to make the main functions more readable

--- Assets/js/myScript.js
+++ Assets/js/myScript.js
@@ -1,42 +1,47 @@
 
 let search = "Bujumbura";
 
-
 init();
-
 
 function init(){
 
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=16da2c71dd8c2c76dfce15f0f75a5dea";
-    
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(function(weather){
+    }).then(function(response){
 
-        // Set up the text for the main header
-        let townName = weather.name;
-        let townCountry = weather.sys.country;
-        let timeStamp = new Date(weather.dt * 1000);
-        timeStamp = moment(timeStamp).format("MM/DD/YYYY");
-        let headerTitle = townName + ", " + townCountry + " " + "(" + timeStamp + ")";
+        // // Set up the text for the main header
+        // let townName = weather.name;
+        // let townCountry = weather.sys.country;
+        // let timeStamp = new Date(weather.dt * 1000);
+        // timeStamp = moment(timeStamp).format("MM/DD/YYYY");
+        // let headerTitle = townName + ", " + townCountry + " " + "(" + timeStamp + ")";
 
-        //Set up the Temp in F
-        let temp = (weather.main.temp - 273.15) * 9/5 + 32;
-        temp = temp.toFixed(2);            
-
+        // //Set up the Temp in F
+        // let temp = (weather.main.temp - 273.15) * 9/5 + 32;
+        // temp = temp.toFixed(2);            
         
-        //Assign Text To Card IDs
-        $("#cityHeader").text(headerTitle);
-        $("#tempText").text("Temp:  "  + temp + "°F");
-        $("#humidityText").text("Humidity: " + weather.main.humidity);
-        $("#windSpeedText").text("WindSpeed: " + weather.wind.speed);
+        // //Assign Text To Card IDs
+        // $("#cityHeader").text(headerTitle);
+        // $("#tempText").text("Temp:  "  + temp + "°F");
+        // $("#humidityText").text("Humidity: " + weather.main.humidity);
+        // $("#windSpeedText").text("WindSpeed: " + weather.wind.speed);
+        setMainWeatherCard(response);
 
-        //define coordinates
-        let lon = weather.coord.lon;
-        let lat = weather.coord.lat;
-        
-        let forecastURL = "https://api.openweathermap.org/data/2.5/onecall?appid=16da2c71dd8c2c76dfce15f0f75a5dea&lat=" + lat + "&lon=" + lon;
+        //Grab The next 5 forecast days
+        let lat = response.coord.lat;
+        let lon = response.coord.lon;
+        getForecast(lat, lon)
+
+    });
+
+}//end init()
+
+function getForecast(lat, lon)
+{
+    let forecastURL = "https://api.openweathermap.org/data/2.5/onecall?appid=16da2c71dd8c2c76dfce15f0f75a5dea&lat=" + lat + "&lon=" + lon;
 
         $.ajax({
             url: forecastURL,
@@ -49,76 +54,37 @@ function init(){
 
             for(let i = 1; i <  6; i++)
             {
-                let theDate = new Date(weatherDays[i].dt * 1000);
-                theDate = moment(theDate).format("MM/DD/YYYY");
-                
-                //create card div
-                let $newCard = $("<div>");
-                $newCard.addClass("card col-sm forecast");
-                $("#forecastDiv").append($newCard);
-                
-                let $newCardBody = $("<div>");
-                $newCardBody.addClass("card-body");
-                $newCard.append($newCardBody);
-
-                let $cardheader = $("<h4>");
-                $cardheader.text(theDate);
-                $cardheader.addClass("lead");
-                $newCardBody.append($cardheader);
-
-                
-                let dayIcon = "http://openweathermap.org/img/wn/" + weatherDays[i].weather[0].icon + ".png";
-
-                let $icon = $("<img>");
-                $icon.addClass("row");
-                $icon.attr("src",dayIcon);
-                $newCardBody.append($icon);
-
-                let dayTemp = (weatherDays[i].temp.day- 273.15) * 9/5 + 32;
-                dayTemp = dayTemp.toFixed(2);
-                
-                let $tempParagraph = $("<p>");
-                $tempParagraph.addClass("card-text");
-                $tempParagraph.text("Temp: " + dayTemp + "°F");
-                $newCardBody.append($tempParagraph);
-
-                let $humidityParagraph = $("<p>");
-                $humidityParagraph.addClass("card-text");
-                $humidityParagraph.text("Humidity: " + weatherDays[i].humidity);
-                $newCardBody.append($humidityParagraph);
-                
+                setForecastCard(weatherDays[i]);          
             }
 
-            
             $("#indexText").text("UV Index: " + forecast.current.uvi);
             let uvNum = Math.floor(forecast.current.uvi);
 
             setUvBackground(uvNum);
-            // console.log(uvNum);
-
-            // if(uvNum >= 0 && uvNum <= 2)
-            // {
-            //     $("#indexText").attr("class", "uvLow");
-            // }
-            // else if(uvNum >= 3 && uvNum <= 5)
-            // {
-            //     $("#indexText").attr("class", "uvModerate");                        
-            // }
-            // else if(uvNum >= 6 && uvNum <= 7)
-            // {
-            //     $("#indexText").attr("class", "uvHigh");
-            // }
-            // else
-            // {
-            //     $("#indexText").attr("class", "uvVeryHigh");
-            // }
-
         })
+}//end getForecast()
 
-    });
+function setMainWeatherCard(weather)
+{
+    console.log(weather)
+    // Set up the text for the main header
+    let townName = weather.name;
+    let townCountry = weather.sys.country;
+    let timeStamp = new Date(weather.dt * 1000);
+    timeStamp = moment(timeStamp).format("MM/DD/YYYY");
+    let headerTitle = townName + ", " + townCountry + " " + "(" + timeStamp + ")";
+
+    //Set up the Temp in F
+    let temp = (weather.main.temp - 273.15) * 9/5 + 32;
+    temp = temp.toFixed(2);            
+    
+    //Assign Text To Card IDs
+    $("#cityHeader").text(headerTitle);
+    $("#tempText").text("Temp:  "  + temp + "°F");
+    $("#humidityText").text("Humidity: " + weather.main.humidity);
+    $("#windSpeedText").text("WindSpeed: " + weather.wind.speed);
 
 }
-
 
 function setUvBackground(uNum)
 {
@@ -127,16 +93,15 @@ function setUvBackground(uNum)
     // 3 to 5	Yellow	"Moderate"
     // 6 to 7	Orange	"High"
     // 8 to 10	Red	"Very high"
-    console.log(uvNum);
-    if(uvNum >= 0 && uvNum <= 2)
+    if(uNum >= 0 && uNum <= 2)
     {
         $("#indexText").attr("class", "uvLow");
     }
-    else if(uvNum >= 3 && uvNum <= 5)
+    else if(uNum >= 3 && uNum <= 5)
     {
         $("#indexText").attr("class", "uvModerate");                        
     }
-    else if(uvNum >= 6 && uvNum <= 7)
+    else if(uNum >= 6 && uNum <= 7)
     {
         $("#indexText").attr("class", "uvHigh");
     }
@@ -144,32 +109,80 @@ function setUvBackground(uNum)
     {
         $("#indexText").attr("class", "uvVeryHigh");
     }
-}
+}//End setUvBackground()
 
+function setForecastCard(day)
+{
+    let theDate = new Date(day.dt * 1000);
+    theDate = moment(theDate).format("MM/DD/YYYY");
+    
+    //create card div
+    let $newCard = $("<div>");
+    $newCard.addClass("card col-sm forecast");
+    $("#forecastDiv").append($newCard);
+    
+    let $newCardBody = $("<div>");
+    $newCardBody.addClass("card-body");
+    $newCard.append($newCardBody);
 
+    let $cardheader = $("<h4>");
+    $cardheader.text(theDate);
+    $cardheader.addClass("lead");
+    $newCardBody.append($cardheader);
 
+    
+    let dayIcon = "http://openweathermap.org/img/wn/" + day.weather[0].icon + ".png";
 
+    let $icon = $("<img>");
+    $icon.addClass("row");
+    $icon.attr("src",dayIcon);
+    $newCardBody.append($icon);
 
+    let dayTemp = (day.temp.day- 273.15) * 9/5 + 32;
+    dayTemp = dayTemp.toFixed(2);
+    
+    let $tempParagraph = $("<p>");
+    $tempParagraph.addClass("card-text");
+    $tempParagraph.text("Temp: " + dayTemp + "°F");
+    $newCardBody.append($tempParagraph);
 
+    let $humidityParagraph = $("<p>");
+    $humidityParagraph.addClass("card-text");
+    $humidityParagraph.text("Humidity: " + day.humidity);
+    $newCardBody.append($humidityParagraph);
+}//End setForecastCard()
 
-
-
-
-
-
-$("#citiesList").on("click", "li", function(){
+function cityClick()
+{
     search = $(this).data("search");
     console.log(search);
     init();
-});
+}//End cityClick()
 
-
-$("#addBtn").click(function(){
+function addCityLI()
+{
     let $listItem = $("<li>");
+
     let listText = $("#cityInput").val();
     listText = listText.trim();
+
     $listItem.text(listText);
     $listItem.addClass("list-group-item");
     $listItem.attr("data-search", $("#cityInput").val())
     $("#citiesList").append($listItem);
-});
+    $("#cityInput").val("");
+}//end addCityLI()
+
+function checkForEnter(event)
+{
+    if(event.key === "Enter")
+        addCityLI();
+}//end Check For Enter
+
+
+//Event Listeners
+$("#citiesList").on("click", "li", cityClick);
+
+$("#addBtn").click(addCityLI);
+
+$("#cityInput").keypress(checkForEnter);
