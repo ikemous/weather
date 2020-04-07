d1462273c707f1533ea5db4f93abf3d145d9f91d
Ike Barranco <barranco.ike@outlook.com>
2020-04-07 04:17:01 -0600

    added comments to multiple functions in the js also added some modifications to the index to display a icon of the waether for the day

--- Assets/js/myScript.js
+++ Assets/js/myScript.js
@@ -1,33 +1,33 @@
 
-let search = "Bujumbura";
+/**
+ * myScript.js
+ * Author: Adrian Ike Barranco
+ * Date: 20200407
+ */
+
+//Gobal Variables
+let search = "Bujumbura";//Bujumbura Used for the inital card Create
 
 init();
 
+/**
+ *              init()
+ *  Purpose: To Grab Weather from API to update the cards
+ *  Parameters: None
+ *  Returns: None
+ */
 function init(){
 
+    //Initialize the URL to Query
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=16da2c71dd8c2c76dfce15f0f75a5dea";
-    console.log(queryURL);
+
+    //Grab the weather information from the API
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).then(function(response){
 
-        // // Set up the text for the main header
-        // let townName = weather.name;
-        // let townCountry = weather.sys.country;
-        // let timeStamp = new Date(weather.dt * 1000);
-        // timeStamp = moment(timeStamp).format("MM/DD/YYYY");
-        // let headerTitle = townName + ", " + townCountry + " " + "(" + timeStamp + ")";
-
-        // //Set up the Temp in F
-        // let temp = (weather.main.temp - 273.15) * 9/5 + 32;
-        // temp = temp.toFixed(2);            
-        
-        // //Assign Text To Card IDs
-        // $("#cityHeader").text(headerTitle);
-        // $("#tempText").text("Temp:  "  + temp + "°F");
-        // $("#humidityText").text("Humidity: " + weather.main.humidity);
-        // $("#windSpeedText").text("WindSpeed: " + weather.wind.speed);
+        //update Display for main card
         setMainWeatherCard(response);
 
         //Grab The next 5 forecast days
@@ -39,34 +39,51 @@ function init(){
 
 }//end init()
 
+/**
+ *              getForecast()
+ *  Purpose: To Grab the 7 day forecast from the API and update the Cards
+ *  Parameters: lat and lon - used to help initialize query URL to obtain weather information
+ *  Returns: None
+ */
 function getForecast(lat, lon)
 {
+    //Initialize the Query URL
     let forecastURL = "https://api.openweathermap.org/data/2.5/onecall?appid=16da2c71dd8c2c76dfce15f0f75a5dea&lat=" + lat + "&lon=" + lon;
 
-        $.ajax({
-            url: forecastURL,
-            method: 'GET'
-        }).then(function(forecast){
+    //grab the weather info with the url
+    $.ajax({
+        url: forecastURL,
+        method: 'GET'
+    }).then(function(forecast){
 
-            $("#forecastDiv").empty();
+        //Empty all card divs
+        $("#forecastDiv").empty();
 
-            let weatherDays = forecast.daily;
+        //Grab the forecast days from the API
+        let weatherDays = forecast.daily;
 
-            for(let i = 1; i <  6; i++)
-            {
-                setForecastCard(weatherDays[i]);          
-            }
+        //Update All weather cards
+        for(let i = 1; i <  6; i++)
+        {
+            setForecastCard(weatherDays[i]);          
+        }
 
-            $("#indexText").text("UV Index: " + forecast.current.uvi);
-            let uvNum = Math.floor(forecast.current.uvi);
-
-            setUvBackground(uvNum);
-        })
+        //Grab the UV information and set the background
+        $("#indexText").text(forecast.current.uvi);
+        let uvNum = Math.floor(forecast.current.uvi);
+        setUvBackground(uvNum);
+    })
 }//end getForecast()
 
+/**
+ *              setMainWeatherCard()
+ *  Purpose: Update the Main Card With The right Weather Information
+ *  Parameters: weather - object holding all information for the weather location
+ *  Returns: None
+ */
 function setMainWeatherCard(weather)
 {
-    console.log(weather)
+    console.log(weather);
     // Set up the text for the main header
     let townName = weather.name;
     let townCountry = weather.sys.country;
@@ -77,35 +94,40 @@ function setMainWeatherCard(weather)
     //Set up the Temp in F
     let temp = (weather.main.temp - 273.15) * 9/5 + 32;
     temp = temp.toFixed(2);            
+
+    let dayIcon = "http://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png";
     
     //Assign Text To Card IDs
     $("#cityHeader").text(headerTitle);
     $("#tempText").text("Temp:  "  + temp + "°F");
     $("#humidityText").text("Humidity: " + weather.main.humidity);
     $("#windSpeedText").text("WindSpeed: " + weather.wind.speed);
+    $("#mainCardIcon").attr("src", dayIcon);
 
-}
+}//End setMainWeatherCard()
 
+/**
+ *              setUvBackground()
+ *  Purpose: Update The UV background with the Danger Level of the UV index
+ *  Parameters: uNum - whole number of current UV index
+ *  Returns: None
+ */
 function setUvBackground(uNum)
 {
     //UV index	Media graphic color	Risk of harm from unprotected Sun exposure, for the average adult
-    // 0 to 2	Green	"Low"
-    // 3 to 5	Yellow	"Moderate"
-    // 6 to 7	Orange	"High"
-    // 8 to 10	Red	"Very high"
-    if(uNum >= 0 && uNum <= 2)
+    if(uNum >= 0 && uNum <= 2)// 0 to 2	Green	"Low"
     {
         $("#indexText").attr("class", "uvLow");
     }
-    else if(uNum >= 3 && uNum <= 5)
+    else if(uNum >= 3 && uNum <= 5)// 3 to 5	Yellow	"Moderate"
     {
         $("#indexText").attr("class", "uvModerate");                        
     }
-    else if(uNum >= 6 && uNum <= 7)
+    else if(uNum >= 6 && uNum <= 7)// 6 to 7	Orange	"High"
     {
         $("#indexText").attr("class", "uvHigh");
     }
-    else
+    else// 8 to 10	Red	"Very high"
     {
         $("#indexText").attr("class", "uvVeryHigh");
     }
@@ -113,6 +135,7 @@ function setUvBackground(uNum)
 
 function setForecastCard(day)
 {
+
     let theDate = new Date(day.dt * 1000);
     theDate = moment(theDate).format("MM/DD/YYYY");
     
